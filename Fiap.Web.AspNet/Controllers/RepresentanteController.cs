f4f6782d854376caabd0066537123c8f86136ee1
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2023-09-05 13:50:39 -0300

    Primeiro Commit

---
+++ Fiap.Web.AspNet/Controllers/RepresentanteController.cs
@@ -0,0 +1,27 @@
+﻿using Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet.Controllers
+{
+    public class RepresentanteController : Controller
+    {
+
+        private IList<RepresentanteModel> representantes;
+
+        public RepresentanteController()
+        {
+            // De uma forma rudimentar, podemos dizer que esse bloco de código 
+            // simula o retorno de uma consulta no banco de dados
+            representantes = new List<RepresentanteModel>();
+            representantes.Add(new RepresentanteModel(1, "444.143.658-05", "José Carlos Silva"));
+            representantes.Add(new RepresentanteModel(2, "062.572.723-19", "Maria José Fernandes"));
+            representantes.Add(new RepresentanteModel(2, "920.680.661-06", "Carlos Silva"));
+        }
+
+        public IActionResult Index()
+        {
+            // Retornando para View a lista de Representantes
+            return View(representantes); // <-- Atenção
+        }
+    }
+}
